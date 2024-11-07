8d25c6fa873ed92a4b0212b53c9aca5b46491838
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-11-07 21:27:48 +0200

    fixed booking schema

--- flight-booking-backend/models/Booking.js
+++ flight-booking-backend/models/Booking.js
@@ -1,11 +1,15 @@
 // models/Booking.js
 const mongoose = require('mongoose');
 
+// Define the schema for bookings
 const bookingSchema = new mongoose.Schema({
-  flightName: String,  // Store the flight name
-  // Add other fields for booking (e.g., user information, date, etc.)
+  bookedName: { type: String, required: true },
+  bookedDeparture: { type: String, required: true },
+  bookedDestination: { type: String, required: true },
+  bookedPrice: { type: Number, required: true }
 });
 
+// Create the Booking model based on the schema
 const Booking = mongoose.model('Booking', bookingSchema);
 
 module.exports = Booking;
