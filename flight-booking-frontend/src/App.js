b803204a951e7130dd7e969e86b08954522a86ee
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-11-08 03:00:10 +0200

    added a route to the home page

--- flight-booking-frontend/src/App.js
+++ flight-booking-frontend/src/App.js
@@ -11,6 +11,9 @@ const App = () => {
         <nav className="bg-blue-600 p-4">
           <ul className="flex justify-center space-x-6">
             <li>
+              <Link to="/" className="text-white text-xl font-semibold hover:text-blue-200">Home</Link>
+            </li>
+            <li>
               <Link to="/flights" className="text-white text-xl font-semibold hover:text-blue-200">Available Flights</Link>
             </li>
             <li>
@@ -27,15 +30,15 @@ const App = () => {
             path="/" 
             element={
               <div className="text-center py-16 pb-24">
-  <div className="inline-block text-4xl font-extrabold mb-6">
-    <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-400 via-pink-500 to-purple-600 animate-pulse tracking-wide">
-      Welcome to the Flight Booking App!
-    </span>
-  </div>
-  <div>
-    <span className="text-6xl text-indigo-600">🌍</span>
-  </div>
-</div>
+                <div className="inline-block text-4xl font-extrabold mb-6">
+                  <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-400 via-pink-500 to-purple-600 animate-pulse tracking-wide">
+                    Welcome to the Flight Booking App!
+                  </span>
+                </div>
+                <div>
+                  <span className="text-6xl text-indigo-600">🌍</span>
+                </div>
+              </div>
             } 
           />
         </Routes>
