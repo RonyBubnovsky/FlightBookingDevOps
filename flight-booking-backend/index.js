62265fd1f716f48eb264eed536d111458c1ee6f0
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-11-09 23:23:05 +0200

    ss

--- flight-booking-backend/index.js
+++ flight-booking-backend/index.js
@@ -5,7 +5,7 @@ const flightRoutes = require('./routes/flightroutes');
 const bookingRoutes = require('./routes/bookingRoutes');
 const seedFlights = require('./seedFlights'); // Import the seed file
 
-require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3001;  // Fix the PORT assignment
 
@@ -22,7 +22,7 @@ app.use('/api/flights', flightRoutes);
 app.use('/api/bookings', bookingRoutes);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect('mongodb+srv://ronybubnovsky:dLJgXWD3YpqPDqBG@flightbooking.4cfj2.mongodb.net/?retryWrites=true&w=majority&appName=FlightBooking')
   .then(() => {
     console.log('Connected to MongoDB');
 
