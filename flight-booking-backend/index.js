1d86afaa58e953961f47a617f820db87ba8bfc28
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-11-10 01:46:55 +0200

    againininin

--- flight-booking-backend/index.js
+++ flight-booking-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const flightRoutes = require('./routes/flightRoutes');
+const flightRoutes = require('./routes/flightroutes');
 const bookingRoutes = require('./routes/bookingRoutes');
 const seedFlights = require('./seedFlights'); // Import the seed file
 
