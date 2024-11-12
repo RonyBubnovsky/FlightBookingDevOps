68934f26dcc1e93ed438a8eb67f8122a8a461c64
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-11-12 19:41:59 +0200

    fsfsfsf

--- flight-booking-frontend/cypress.config.js
+++ flight-booking-frontend/cypress.config.js
@@ -3,14 +3,14 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    screenshotOnRunFailure: false,
-    video: false,
-    retries: 4,
-    defaultCommandTimeout: 15_000,
-    waitForAnimations: true,
-    responseTimeout: 120_000,
+    headless: true, // Run Cypress in headless mode
+    video: false, // Disable video recording
+    retries: 4, // Set a reasonable retry limit
+    defaultCommandTimeout: 10000, // Adjust timeout as needed
+    waitForAnimations: false, // Disable waiting for animations to speed up tests
+    responseTimeout: 60000, // Adjust timeout as needed
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implement node event listeners here
     },
   },
 })
