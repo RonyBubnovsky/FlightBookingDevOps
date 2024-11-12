b66b1efbe314ff349ac1871a5850cec8e15212b1
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-11-12 18:46:26 +0200

    sdsdsdsd

--- flight-booking-frontend/cypress/e2e/availableFlightsPageTest.cy.js
+++ flight-booking-frontend/cypress/e2e/availableFlightsPageTest.cy.js
@@ -1,11 +1,16 @@
 describe('Flight Booking Page', () => {
-  beforeEach(() => {
-    // Visit the flights page
-    cy.visit('http://localhost:3000/flights');
+    beforeEach(() => {
+      // Visit the flights page
+      cy.visit('http://localhost:3000/flights');
+    });
+  
+    it('should book a flight successfully', () => {
+      // Click the "Book Flight" button for the first flight
+      cy.get("[data-cy='bookflight_button']").first().click();
+  
+      // Assert that the success SweetAlert message appears
+      cy.get('.swal2-title').should('have.text', 'Success!');
+      cy.get('.swal2-confirm').click();
+  
+    });
   });
-
-  it('should open the flights page successfully', () => {
-    // Assert that the page contains the title or other specific text
-    cy.url().should('include', '/flights'); // Ensure the URL is correct
-  });
-});
