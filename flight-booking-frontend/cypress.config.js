bea15778f209245e3698641e6df8aa980b79b3fb
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-11-12 19:47:21 +0200

    hhhhh

--- flight-booking-frontend/cypress.config.js
+++ flight-booking-frontend/cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    headless: true, // Run Cypress in headless mode
     video: false, // Disable video recording
     retries: 4, // Set a reasonable retry limit
     defaultCommandTimeout: 10000, // Adjust timeout as needed
